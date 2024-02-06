325a0b2836b4ded4c8fac76b155bcb021c383829
ryandwi <r_y4ns@yahoo.com>
2024-02-06 12:00:34 +0700

    CHECK

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-       <p>PT KMJ2</p>
+       <p>PT KANINDO MAKMUR JAYA 2</p>
     </main>
   );
 }
